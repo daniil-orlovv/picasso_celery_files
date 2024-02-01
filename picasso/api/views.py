060cd26dcece18353b1c663c09c691d57399ba73
Даниил Орлов <danilorlov9898@yandex.ru>
2024-02-01 21:42:46 +0300

    Добавил два класса APIView для двух эндпоинтов

--- picasso/api/views.py
+++ picasso/api/views.py
@@ -1,9 +1,24 @@
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from files.models import File
 from api.serializers import FileSerializer
 
 
-class FileViewSet(viewsets.ModelViewSet):
-    queryset = File.objects.all()
-    serializer_class = FileSerializer
+class FilesAPIList(APIView):
+
+    def get(self, request):
+        cats = File.objects.all()
+        serializer = FileSerializer(cats, many=True)
+        return Response(serializer.data)
+
+
+class FilesAPICreate(APIView):
+
+    def post(self, request):
+        serializer = FileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
