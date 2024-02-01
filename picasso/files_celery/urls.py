f9747e5bfb51a9bffa2799835d99c76209064cd8
Даниил Орлов <danilorlov9898@yandex.ru>
2024-02-01 21:52:12 +0300

    Проверил работу API - эндпоинт files отвечает 200

--- picasso/files_celery/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from rest_framework.routers import SimpleRouter
-
-router = SimpleRouter()
-
-router.register('upload', ..., basename='upload')
-router.register('files', ..., basename='files')
--- picasso/picasso/settings.py
+++ picasso/picasso/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'files_celery.apps.FilesCeleryCatalog',
+    'files.apps.FilesConfig',
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
