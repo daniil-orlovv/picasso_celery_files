161e29c818049547175009112686247bf8218390
Даниил Орлов <danilorlov9898@yandex.ru>
2024-02-02 20:59:16 +0300

    Добавил обработку исключений

--- api/service.py
+++ api/service.py
@@ -1,9 +1,15 @@
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+
 from files.models import File
 
 
 def update(file_id, new_value):
-    file_obj = File.objects.get(pk=file_id)
-    print(f"Updating processed field for file_id {file_id} to {new_value}")
-    file_obj.processed = new_value
-    file_obj.save()
-    return f"Значение поля файла успешно обновлено: {file_obj}"
+    try:
+        file_obj = get_object_or_404(File, pk=file_id)
+        print(f"Updating processed field for file_id {file_id} to {new_value}")
+        file_obj.processed = new_value
+        file_obj.save()
+        return f"Значение поля файла успешно обновлено: {file_obj}"
+    except Http404:
+        return f"Файл с id {file_id} не найден в базе данных."
