be0524b3fdf8c7ec654ad3d19217a504bca9a32e
Даниил Орлов <danilorlov9898@yandex.ru>
2024-02-02 19:52:02 +0300

    Обновил параметры полей в модели

--- picasso/files/models.py
+++ picasso/files/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class File(models.Model):
     file = models.FileField()
-    uploaded_at = models.DateTimeField()
-    processed = models.BooleanField
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    processed = models.BooleanField(default=False)
